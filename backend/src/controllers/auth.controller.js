fabe0137cd75825b68375b1785ceb41e538ef172
nimsco <nimeshjoshi7891@gmail.com>
2026-07-02 10:37:39 +0545

    folder structure setup

--- backend/src/controllers/auth.controller.js
+++
@@ -1,8 +0,0 @@
-const userModel = require('../models/user.model');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-
-const registerUser = async(req, res) => {
-    try {
-        const { username, email, password, dob , gender } = req.body;
-        

ea99e45c55f5032e51228b162160a3ff8fe1e969
nimsco <nimeshjoshi7891@gmail.com>
2026-07-07 12:04:21 +0545

    Fixed bug of temp file not deleting and added logout routes and refresh token routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken";
+import fs from "fs"
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
@@ -51,40 +53,47 @@ const registerUser = asyncHandler(async (req, res) => {
     if (password.length <= 4)
         throw new ApiError(400, "Password must be at least 4 characters long.");
 
-    const userExists = await User.findOne({
-        $or: [{ username }, { email }],
-    });
-
-    if (userExists) throw new ApiError(409, "User already exists");
-
-    let avatarUrl = null;
-
-    if (req.file?.path) {
-        const avatar = await uploadOnCloudinary(req.file.path);
-        avatarUrl = avatar.secure_url;
-    }
-
-    const user = await User.create({
-        email: email.toLowerCase(),
-        username: username.toLowerCase(),
-        password,
-        dob,
-        gender,
-        avatar: avatarUrl,
-    });
-
-    const createdUser = await User.findById(user._id).select(
-        "-password -refreshToken",
-    );
-
-    if (!createdUser)
-        throw new ApiError(500, "Something went wrong while registering user");
-
-    return res
-        .status(201)
-        .json(
-            new ApiResponse(201, createdUser, "User registered successfully"),
+try {
+        const userExists = await User.findOne({
+            $or: [{ username }, { email }],
+        });
+    
+        if (userExists) throw new ApiError(409, "User already exists");
+    
+        let avatarUrl = null;
+    
+        if (req.file?.path) {
+            const avatar = await uploadOnCloudinary(req.file.path);
+            avatarUrl = avatar.secure_url;
+        }
+    
+        const user = await User.create({
+            email: email.toLowerCase(),
+            username: username.toLowerCase(),
+            password,
+            dob,
+            gender,
+            avatar: avatarUrl,
+        });
+    
+        const createdUser = await User.findById(user._id).select(
+            "-password -refreshToken",
         );
+    
+        if (!createdUser)
+            throw new ApiError(500, "Something went wrong while registering user");
+    
+        return res
+            .status(201)
+            .json(
+                new ApiResponse(201, createdUser, "User registered successfully"),
+            );
+} catch (error) {
+    if (req.file?.path && fs.existsSync(req.file.path)){
+        fs.unlinkSync(req.file.path)
+    }
+    throw new ApiError(error.statusCode || 500, error?.message || "Something went wrong while registering user")
+}
 });
 
 const loginUser = asyncHandler(async (req, res) => {
@@ -159,4 +168,50 @@ const logoutUser = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, {}, "User logged out"));
 });
 
-export { registerUser, loginUser, logoutUser };
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incommingRequestToken =
+        req.cookies.refreshToken || req.body.refreshToken;
+
+    if (!incommingRequestToken) throw new ApiError(401, "Unauthorized request");
+
+    try {
+        const decodedToken = jwt.verify(
+            incommingRequestToken,
+            process.env.REFRESH_TOKEN_SECRET,
+        );
+
+        const user = await User.findById(decodedToken?._id);
+
+        if (!user) throw new ApiError(401, "Invalid refresh token");
+
+        if (incommingRequestToken !== user?.refreshToken)
+            throw new ApiError(401, "Refresh token is expired or used");
+
+        const options = {
+            httpOnly: true,
+            secure: true,
+        };
+
+        const { accessToken, newRefreshToken } =
+            await generateAccessAndRefreshTokens(user._id);
+
+        return res
+            .status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", newRefreshTokenefreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        accessToken,
+                        refreshToken: newRefreshToken,
+                    },
+                    "Access token refreshed",
+                ),
+            );
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token");
+    }
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
